624b7e42151f8bcc20c76a3e7ede82ebc65fefea
HARSHIT SINGH$ <harshitsingh2004hardaspur@gmail.com>
2025-11-28 22:14:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ let b=4;
 console.log(b);
 const sum =a+b;
 console.log(`sum is${sum}`);
+let num = prompt("Enter a number");
+if (num%5==0) {
+    console.log(num + " is divisible by 5");
+}
+else {
+    console.log(num + " is not divisible by 5");
+}
+
