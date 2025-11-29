b333bd7b81bc090cdc9dee5ee936e036132da0c4
HARSHIT SINGH$ <harshitsingh2004hardaspur@gmail.com>
2025-11-29 21:12:36 +0530

    Add logical and comparison operations in script2.js

--- script2.js
+++ script2.js
@@ -1,3 +1,9 @@
 let a=6;
 let b=7;
 console.log(a+b);
+let con1 =b>a;//true
+let con2 = a===b;//false
+console.log("con1 && con2", con1&&con2);//false
+console.log("con1 || con2", con1||con2);//true
+console.log("!con1", !con1);//false
+console.log("!con2", !con2);//true
