5530ad5247bb9af4bb41d9887ccff96fab41e9fb
HARSHIT SINGH$ <harshitsingh2004hardaspur@gmail.com>
2025-11-30 19:10:20 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -3,6 +3,7 @@ let b=7;
 console.log(a+b);
 let con1 =b>a;//true
 let con2 = a===b;//false
+console.log(con1);//true
 console.log("con1 && con2", con1&&con2);//false
 console.log("con1 || con2", con1||con2);//true
 console.log("!con1", !con1);//false
