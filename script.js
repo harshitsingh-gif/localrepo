593c29aa234a9feb8bc969bdcbcb81552fffc051
HARSHIT SINGH$ <harshitsingh2004hardaspur@gmail.com>
2025-12-01 21:45:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,4 +11,10 @@ if (num%5==0) {
 else {
     console.log(num + " is not divisible by 5");
 }
-
+let num = prompt("Enter a number");
+if (num%5==0) {
+    console.log(num + " is divisible by 5");
+}
+else {
+    console.log(num + " is not divisible by 5");
+}
