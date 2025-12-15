27cde448cf7ab82207d27fa285616982ee565d9e
HARSHIT SINGH$ <harshitsingh2004hardaspur@gmail.com>
2025-12-15 23:03:30 +0530

    Remove variable declarations and console logs

--- script2.js
+++ script2.js
@@ -1,13 +1,1 @@
-let a=6;
-let b=7;
-console.log(a+b);
-let con1 =b>a;//true
-let con2 = a===b;//false
-console.log(con1);//true
-console.log("con1 && con2", con1&&con2);//false
-console.log("con1 || con2", con1||con2);//true
-console.log("!con1", !con1);//false
-console.log("!con2", !con2); //true
-let print= ()=>{
-  console.log("hello!");
-}
+
