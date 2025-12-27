ea9a14a867205c3cab05775935bde5543c0c1577
HARSHIT SINGH$ <harshitsingh2004hardaspur@gmail.com>
2025-12-27 16:35:26 +0530

    Fix console log formatting and update item values

--- script.js
+++ script.js
@@ -53,8 +53,8 @@ else if(score>=0 && score<=49){
 let items =[250,645,300,900,50];
  let i=0;
  for(let val of items){
-    console.log(`value at index ${i} =${val}`);
+console.log(`value at index ${i} =${val}`);
  let offer =val/10;
-         items[i]-=offer;
-         console.log(`value after offer=${items[i]}`);
-         i++;
+  items[i]-=offer;
+ console.log(`value after offer=${items[i]}`);
+ i++;
