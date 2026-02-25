206b55052aa2637b977563e47e8f3a310e1547e1
HARSHIT SINGH$ <harshitsingh2004hardaspur@gmail.com>
2026-02-25 21:20:56 +0530

    Implement divisibility check for user input
    Added functionality to check if a number is divisible by 5.

--- script.js
+++ script.js
@@ -5,15 +5,21 @@ console.log(b);
 const sum =a+b;
 console.log(`sum is${sum}`);
 
+
 let num = prompt("Enter a number");
+
 
 if (num%5==0) {
     console.log(num + " is divisible by 5");
+    
 }
 else {
     console.log(num + " is not divisible by 5");
+    
 }
 let num = prompt("Enter a number");
+
+
 
 if (num%5==0) {
     console.log(num + " is divisible by 5");
